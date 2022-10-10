5baef53ccd2061f217c4814b5ed082b6e6a38ab7
Yehuthith <yehuthith@gmail.com>
2022-10-10 14:55:26 +0300

    💄 initialize style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,25 @@
+<script>
+  import GlobalStyle from "../style/global.svelte";
+
+</script>
 <svelte:head>
     <title>Layla El-Sekaifi</title>
 </svelte:head>
 
+<header>
 <h1>Layla El-Sekaifi</h1>
+</header>
 
 <main>
 <slot></slot>
 </main>
+
+<GlobalStyle />
+
+<style lang="scss">
+:root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
---
+++ src/style/global.svelte
@@ -0,0 +1,4 @@
+<style lang="scss" global>
+@import "css_reset.scss";
+@import "theme.scss";
+</style>
