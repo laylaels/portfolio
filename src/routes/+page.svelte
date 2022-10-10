b590b4af3b6651efe5b651c8b3cea052fbfe681a
Yehuthith <yehuthith@gmail.com>
2022-10-10 18:15:55 +0300

    💬 replace homepage text with placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,3 @@
-<script>
-  import BeforeAfter from "../component/beforeAfter.svelte";
-</script>
-
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ut? Eaque ipsa
-  doloribus ab suscipit. Voluptatibus eos corrupti eveniet quaerat, asperiores
-  sit! Impedit sit nostrum tenetur, molestias nam culpa maiores.
-</p>
-
-<BeforeAfter
-  before="https://static.wixstatic.com/media/dc5a07_056e8d5e661f4129a8d9ffa859c6bcf8~mv2.jpg/v1/fill/w_600,h_400,q_85,usm_0.66_1.00_0.01/dc5a07_056e8d5e661f4129a8d9ffa859c6bcf8~mv2.jpg"
-  after="https://static.wixstatic.com/media/dc5a07_94ff45cc674646349e2cc89b8e0699a4~mv2.jpg/v1/fill/w_600,h_401,q_85,usm_0.66_1.00_0.01/dc5a07_94ff45cc674646349e2cc89b8e0699a4~mv2.jpg"
-  alt="Test"
-  options={{ addCircle: true, addCircleBlur: true }}
-/>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, nobis voluptatibus
+vero placeat exercitationem dolorem reiciendis sit rem perferendis nesciunt sed
+sapiente qui eos laborum possimus minima suscipit ex dignissimos.
