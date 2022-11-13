2e57e37b43c218b010f84e72ac69caddccd5a19f
Yehuthi <yehuthith@gmail.com>
2022-11-13 07:39:05 +0200

    Initialize homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, nobis voluptatibus
-vero placeat exercitationem dolorem reiciendis sit rem perferendis nesciunt sed
-sapiente qui eos laborum possimus minima suscipit ex dignissimos.
+<script lang="ts">
+	const AGE =
+		new Date(
+			new Date().valueOf() - new Date("2000-07-14").valueOf()
+		).getUTCFullYear() - 1970;
+	const LOCATION = "West London";
+</script>
+
+<h2>Hi!</h2>
+<p>
+	I'm Layla El-Sekaifi, a {AGE} years-old creative and graphics editor from {LOCATION}.
+</p>
