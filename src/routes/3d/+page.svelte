b4ad82b994e379a2e077534a915260ba70dc17d3
Yehuthi <yehuthith@gmail.com>
2022-11-13 07:15:05 +0200

    Populate 3D page

--- src/routes/3d/+page.svelte
+++ src/routes/3d/+page.svelte
@@ -1,1 +1,28 @@
-TODO
+<script lang="ts">
+	import { base } from "$app/paths";
+	import BeforeAfter from "../../component/beforeAfter.svelte";
+
+	const assets = [
+		{ title: "Coffee Set", file: "coffee_set.png" },
+		{ title: "Cups", file: "cups.png" },
+		{ title: "Jug", file: "jug.png" },
+	];
+
+	const virtual_staging_count = 7;
+</script>
+
+<h2>3D</h2>
+
+<h3>Virtual Staging</h3>
+{#each Array(virtual_staging_count) as _, i}
+	<BeforeAfter
+		alt="Virtual Staging"
+		before="{base}/img/3d/vstage/{i}b.jpg"
+		after="{base}/img/3d/vstage/{i}a.jpg"
+	/>
+{/each}
+
+<h3>Assets</h3>
+{#each assets as { title, file }}
+	<img src="{base}/img/3d/asset/{file}" alt={title} />
+{/each}
