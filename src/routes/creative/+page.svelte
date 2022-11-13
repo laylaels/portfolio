61b9380a9de1fbf9eff79c85eacd4375b95aa2f2
Yehuthi <yehuthith@gmail.com>
2022-11-13 17:03:54 +0200

    Add art

--- src/routes/creative/+page.svelte
+++ src/routes/creative/+page.svelte
@@ -1,1 +1,9 @@
-TODO
+<script>
+	import { base } from "$app/paths";
+</script>
+
+<h2>Creative</h2>
+<h3>Art</h3>
+{#each ["lj", "c", "i", "f", "a", "b"] as image}
+	<img src="{base}/img/creative/art/{image}.webp" alt="Artwork" />
+{/each}
