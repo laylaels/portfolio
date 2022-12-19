2b88ce6cd41235bb2ca6afe567cb15b086172149
Yehuthi <yehuthith@gmail.com>
2022-12-19 20:55:26 +0200

    Add brand logos

---
+++ src/component/brand.svelte
@@ -0,0 +1,96 @@
+<script lang="ts" context="module">
+	export interface Brand {
+		id: string;
+		name: string;
+		abbreviation?: string;
+		color: string;
+	}
+
+	export const blender: Brand = {
+		id: "blender",
+		name: "Blender",
+		color: "#ea7600",
+	};
+
+	export const photoshop: Brand = {
+		id: "photoshop",
+		name: "Photoshop",
+		abbreviation: "Ps",
+		color: "#31a8ff",
+	};
+
+	export const lightroom: Brand = {
+		id: "lightroom",
+		name: "Lightroom",
+		abbreviation: "Lr",
+		color: "#31a8ff",
+	};
+
+	export const premiere: Brand = {
+		id: "premiere",
+		name: "Premiere",
+		abbreviation: "Pr",
+		color: "#9999ff",
+	};
+
+	export const after_effects: Brand = {
+		id: "after_effects",
+		name: "After Effects",
+		abbreviation: "Ae",
+		color: "#9999FF",
+	};
+
+	export const indesign: Brand = {
+		id: "indesign",
+		name: "InDesign",
+		abbreviation: "Id",
+		color: "#ff3366",
+	};
+
+	export const illustrator: Brand = {
+		id: "illustrator",
+		name: "Illustrator",
+		abbreviation: "Ai",
+		color: "#ff9a00",
+	};
+
+	export const adobe: Brand = {
+		id: "adobe",
+		name: "Adobe",
+		color: "#fa0f00",
+	};
+
+	export enum NameForm {
+		None,
+		Abbreviate,
+		Full,
+	}
+	function nameFormFn(nameForm: NameForm, program: Brand): string {
+		return nameForm == NameForm.Full
+			? program.name
+			: nameForm == NameForm.Abbreviate
+			? program.abbreviation ?? program.name
+			: "";
+	}
+</script>
+
+<script lang="ts">
+	export let brand: Brand;
+	export let naming: NameForm | string = NameForm.Full;
+	export let size: string = "1.5em";
+</script>
+
+<img
+	style="width:{size};height:{size}"
+	src="software/{brand.id}.svg"
+	alt="{brand.name} Icon"
+/>
+<span style="color:{brand.color}"
+	>{typeof naming === "string" ? naming : nameFormFn(naming, brand)}</span
+>
+
+<style lang="scss">
+	img {
+		vertical-align: middle;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Brand, * as brand from "../component/brand.svelte";
+
 	const AGE =
 		new Date(
 			new Date().valueOf() - new Date("2000-07-14").valueOf()
@@ -16,18 +18,25 @@
 	video editor based in {LOCATION}.
 </p>
 <p>
-	For the past 8 years I have been familiarising myself with the Adobe Suite,
-	mainly focussing on Photoshop but ranging through to Lr Pr Ae Id.
+	For the past eight years I have been familiarising myself with the <Brand
+		brand={brand.adobe}
+		naming="Adobe Suite"
+	/>, mainly focussing on <Brand brand={brand.photoshop} /> but ranging through to
+	<Brand brand={brand.lightroom} />,
+	<Brand brand={brand.premiere} />,
+	<Brand brand={brand.after_effects} />, and <Brand brand={brand.indesign} />.
 </p>
 <p>
-	I have been working daily with LightRoom and Photoshop since 2020 as a
-	retoucher and designer for real estate clients bringing their properties to
-	life and preparing their marketing materials.
+	I have been working daily with <Brand brand={brand.lightroom} />
+	and <Brand brand={brand.photoshop} /> since 2020 as a retoucher and designer for
+	real estate clients bringing their properties to life and preparing their marketing
+	materials.
 </p>
 <p>
 	As of recent I have taken an interest in 3D, familiarising myself with the
 	software, concepts and creating models in my free time.
 </p>
 <p>
-	When I’m not working you can find me drawing, discovering new things, swimming, and having a good time with friends or family :)
+	When I’m not working you can find me drawing, discovering new things,
+	swimming, and having a good time with friends or family :)
 </p>
