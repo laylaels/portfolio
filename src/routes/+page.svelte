fa2b0b6bf821a3e7d7b7415d1f781a9ea143ebff
Yehuthi <yehuthith@gmail.com>
2022-12-20 15:11:29 +0200

    Initialize homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,64 +13,78 @@
 	<title>Layla El-Sekaifi</title>
 </svelte:head>
 
-<article>
-	<h2>Hi!</h2>
-	<p>
-		I'm Layla El-Sekaifi, a {AGE} years-old multidisciplinary artist, retoucher and
-		video editor based in {LOCATION}.
-	</p>
-	<p>
-		For the past eight years I have been familiarising myself with the <Brand
-			brand={brand.adobe}
-			naming="Adobe Suite"
-		/>, mainly focussing on <Brand brand={brand.photoshop} /> but ranging through
-		to
-		<Brand brand={brand.lightroom} />,
-		<Brand brand={brand.illustrator} />,
-		<Brand brand={brand.premiere} />,
-		<Brand brand={brand.after_effects} />, and <Brand brand={brand.indesign} />.
-	</p>
-	<p>
-		I have been working daily with <Brand brand={brand.lightroom} />
-		and <Brand brand={brand.photoshop} /> since 2020 as a retoucher and designer
-		for real estate clients bringing their properties to life and preparing their
-		marketing materials.
-	</p>
-	<p>
-		As of recent I have taken an interest in 3D, familiarising myself with the
-		software, concepts and creating models in my free time.
-	</p>
-	<p>
-		When I’m not working you can find me drawing, discovering new things,
-		swimming, and having a good time with friends or family :)
-	</p>
-</article>
-<article>
-	<h2>Portfolio</h2>
+<div class="root">
+	<article>
+		<h2>Hi!</h2>
+		<p>
+			I'm Layla El-Sekaifi, a {AGE} years-old multidisciplinary artist, retoucher
+			and video editor based in {LOCATION}.
+		</p>
+		<p>
+			For the past eight years I have been familiarising myself with the <Brand
+				brand={brand.adobe}
+				naming="Adobe Suite"
+			/>, mainly focussing on <Brand brand={brand.photoshop} /> but ranging through
+			to
+			<Brand brand={brand.lightroom} />,
+			<Brand brand={brand.illustrator} />,
+			<Brand brand={brand.premiere} />,
+			<Brand brand={brand.after_effects} />, and <Brand
+				brand={brand.indesign}
+			/>.
+		</p>
+		<p>
+			I have been working daily with <Brand brand={brand.lightroom} />
+			and <Brand brand={brand.photoshop} /> since 2020 as a retoucher and designer
+			for real estate clients bringing their properties to life and preparing their
+			marketing materials.
+		</p>
+		<p>
+			As of recent I have taken an interest in 3D, familiarising myself with the
+			software, concepts and creating models in my free time.
+		</p>
+		<p>
+			When I’m not working you can find me drawing, discovering new things,
+			swimming, and having a good time with friends or family :)
+		</p>
+	</article>
+	<article>
+		<h2>Portfolio</h2>
 
-	<div class="topcats">
-		{#each portfolio.categories as topcat}
-			<div class="topcat">
-				<div class="iconlabel">
-					<img src="category/{topcat.id}.png" alt={topcat.name} />
-					{topcat.name}
-				</div>
-				<div class="cats">
-					{#each topcat.contents as cat}
-						<div class="cat">
-							<div class="iconlabel">
-								<img src="category/{cat.id}.png" alt={cat.name} />
-								{cat.name}
+		<div class="topcats">
+			{#each portfolio.categories as topcat}
+				<div class="topcat">
+					<div class="iconlabel">
+						<img src="category/{topcat.id}.png" alt={topcat.name} />
+						{topcat.name}
+					</div>
+					<div class="cats">
+						{#each topcat.contents as cat}
+							<div class="cat">
+								<div class="iconlabel">
+									<img src="category/{cat.id}.png" alt={cat.name} />
+									{cat.name}
+								</div>
 							</div>
-						</div>
-					{/each}
+						{/each}
+					</div>
 				</div>
-			</div>
-		{/each}
-	</div>
-</article>
+			{/each}
+		</div>
+	</article>
+</div>
 
 <style lang="scss">
+	.root {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+
+		article {
+			max-width: 700px;
+		}
+	}
+
 	p {
 		margin: 1em 0;
 	}
