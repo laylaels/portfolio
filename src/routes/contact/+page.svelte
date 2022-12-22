4266c4aa405faa1ee57cd40ae9d326f7925bfe39
Yehuthi <yehuthith@gmail.com>
2022-12-23 01:34:54 +0200

    Add form in contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,22 @@
-<h2>Contact</h2>
+<div class="root">
+	<iframe
+		title="Contact Form"
+		src="https://docs.google.com/forms/d/e/1FAIpQLSfc1FT0d8-3nHr8rlYJq9wqExcWV3Xr7HuBwEjjjAsLPySpRg/viewform?embedded=true"
+		height="1000"
+		frameborder="0"
+		marginheight="0"
+		marginwidth="0">Loading…</iframe
+	>
+</div>
 
-<div style="text-align:center;font-size: 7em;">🚧</div>
+<style lang="scss">
+	.root {
+		background: white;
+		max-width: 640px;
+		margin: auto;
+	}
+
+	iframe {
+		width: 100%;
+	}
+</style>
