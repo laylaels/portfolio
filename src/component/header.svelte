c667116be3307c3a02f7a16d1244dba69101dd4c
Yehuthi <yehuthith@gmail.com>
2022-12-26 05:57:34 +0200

    Initialize new header component

---
+++ src/component/header.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<div class="root">
+  <div class="title">
+    <h1>Layla El-Sekaifi</h1>
+    <span>Designer</span>
+  </div>
+
+  <nav>
+    <a href="{base}/">About</a>
+    <a href="{base}/portfolio">Portfolio</a>
+    <a href="{base}/contact">Contact</a>
+  </nav>
+</div>
+
+<style lang="scss">
+  @import "../style/palette.scss";
+
+  .root {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
+  }
+
+  .title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    h1 {
+      font-size: 2em;
+    }
+    span {
+      font-size: 1.5em;
+    }
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    gap: 1em;
+
+    flex-grow: 1;
+
+    border: solid $colorFg;
+    border-width: 1px 0;
+    margin: 0 2em;
+    padding: 0.75em 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,67 +1,52 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import Navbar from "../component/navbar.svelte";
-	import Title from "../component/title.svelte";
-	import GlobalStyle from "../style/global.svelte";
+  import Header from "../component/header.svelte";
+  import { onMount } from "svelte";
+  import GlobalStyle from "../style/global.svelte";
 
-	export const ssr = false;
-	export const prerender = true;
+  export const ssr = false;
+  export const prerender = true;
 
-	let scrollY = 0;
-	onMount(() => {
-		window.addEventListener("scroll", () => {
-			scrollY = window.scrollY;
-		});
-	});
+  let scrollY = 0;
+  onMount(() => {
+    window.addEventListener("scroll", () => {
+      scrollY = window.scrollY;
+    });
+  });
 
-	$: miniHeader = scrollY >= 40;
+  $: miniHeader = scrollY >= 40;
 </script>
 
 <svelte:head>
-	<title>Layla El-Sekaifi</title>
+  <title>Layla El-Sekaifi</title>
 </svelte:head>
 
 <header class:mini={miniHeader}>
-	<div class="title"><Title /></div>
-	<nav><Navbar /></nav>
+  <Header />
 </header>
 
 <main>
-	<slot />
+  <slot />
 </main>
 
 <GlobalStyle />
 
 <style lang="scss">
-	header {
-		display: flex;
-		align-items: center;
-		flex-wrap: wrap;
-		margin-block-end: 1em;
-		padding-block: 1em;
-		position: sticky;
-		top: 0;
-		background: #1c1c1c;
-		z-index: 999;
-	}
+  header {
+    flex-wrap: wrap;
+    margin-block-end: 1em;
+    padding-block: 1em;
+    position: sticky;
+    top: 0;
+    background: #1c1c1c;
+    z-index: 999;
+  }
 
-	header.mini {
-		box-shadow: 0 5px 5px #161616;
-	}
+  header.mini {
+    box-shadow: 0 5px 5px #161616;
+  }
 
-	header,
-	header.mini {
-		transition: box-shadow 0.1s;
-	}
-
-	nav {
-		margin: 1em 0;
-		flex: 1;
-	}
-
-	@media only screen and (min-width: 1440px) {
-		.title {
-			margin-inline-start: 3.25em;
-		}
-	}
+  header,
+  header.mini {
+    transition: box-shadow 0.1s;
+  }
 </style>
