efe67e17c23c2408364f69c8f8b5dad3abc1f7ba
Yehuthi <yehuthith@gmail.com>
2022-12-26 09:39:54 +0200

    Updates

---
+++ internal/immutable/chunks/4-37a9b885.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/portfolio/_page.svelte-78769b10.js";export{t as component};
--- internal/immutable/chunks/4-506b3b5b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/portfolio/_page.svelte-84bda32f.js";export{t as component};
